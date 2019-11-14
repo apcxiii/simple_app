de91e568d54af81d00ec9a808bb48ed0472b96be
Erik Vera <apcxiii@gmail.com>
2019-11-14 16:18:39 -0600

    implements task and kvserver (https://github.com/mareksuscak/elixir-kv-store/blob/master/apps/kv_server/lib/kv_server.ex)

--- lib/simple_app.ex
+++ lib/simple_app.ex
@@ -16,8 +16,8 @@ defmodule SimpleApp do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the endpoint when the application starts
-      supervisor(SimpleApp.Endpoint, []),
-      {SimpleApp.Server, [ip, port]},      
+      supervisor(SimpleApp.Endpoint, []),      
+      
       # Start your own worker by calling: SimpleApp.Worker.start_link(arg1, arg2, arg3)
       # worker(SimpleApp.Worker, [arg1, arg2, arg3]),
     ]
@@ -26,6 +26,14 @@ defmodule SimpleApp do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SimpleApp.Supervisor]
     Supervisor.start_link(children, opts)
+
+    children_socket = [
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [port]]),
+    ]
+
+    opts_socket = [strategy: :one_for_one, name: KVServer.Supervisor]
+    Supervisor.start_link(children_socket, opts_socket)
   end
 
   # Tell Phoenix to update the endpoint configuration
