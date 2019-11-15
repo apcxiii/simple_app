552856a59f7bcd4f7aba356496151fd014f2d8f9
Erik Vera <apcxiii@gmail.com>
2019-11-14 18:59:42 -0600

    format code

--- lib/simple_app.ex
+++ lib/simple_app.ex
@@ -8,30 +8,31 @@ defmodule SimpleApp do
     import Supervisor.Spec
     port = Application.get_env(:simple_app, :socket_port)
     Logger.debug("config ===> #{inspect(port)}")
-
-    ip = Application.get_env(:simple_app, :ip)
-    config = [port: port]
-    Logger.debug("ip ===> #{inspect(ip)}")
-    Logger.debug("port ===> #{inspect(port)}")
     # Define workers and child supervisors to be supervised
     children = [
       # Start the endpoint when the application starts
       supervisor(SimpleApp.Endpoint, []),
-      {SimpleApp.Server, [ip, port]},      
       # Start your own worker by calling: SimpleApp.Worker.start_link(arg1, arg2, arg3)
       # worker(SimpleApp.Worker, [arg1, arg2, arg3]),
+    ]
+
+    children_tcp = [
+      {DynamicSupervisor, strategy: :one_for_one, name: SimpleApp.Handler.DynamicSupervisor},
+      {Task, fn -> SimpleApp.TcpExample.accept(port) end}
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SimpleApp.Supervisor]
     Supervisor.start_link(children, opts)
+
+    opts_tcp = [strategy: :one_for_one, name: SimpleApp.TcpExample.Supervisor]
+    Supervisor.start_link(children_tcp, opts_tcp)
   end
 
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
-    SimpleApp.Endpoint.config_change(changed, removed)
-    :ok
+    SimpleApp.Endpoint.config_change(changed, removed)    
   end
 end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,52 +1,50 @@
 defmodule SimpleApp.UserController do
-	use SimpleApp.Web, :controller
-	alias SimpleApp.Server
-	require Logger
+  use SimpleApp.Web, :controller
+  require Logger
 
-	def create(conn, params) do
-		Logger.debug("params ====> #{inspect(params)}")
+  def create(conn, params) do
+    Logger.debug("params ====> #{inspect(params)}")
 
-		user_json_schema = generate_user_json_schema()
-		user_json_schema |> ExJsonSchema.Schema.resolve
-		
-		case ExJsonSchema.Validator.validate(user_json_schema, params, error_formatter: SimpleApp.JsonErrorFormatter) do
-			:ok -> 
-				port = Application.get_env(:simple_app, :socket_port)
-				{:ok, socket} = :gen_tcp.connect('127.0.0.1', port, [:binary, packet: 0])
-				Logger.debug("socket ===> #{inspect(socket)}")
-    		message = :gen_tcp.send(socket, params["name"] <> " " <> params["last_name"])
-    		Logger.debug("message ===> #{inspect(message)}")
-    		#_ = :gen_tcp.close(socket)
-    
-				conn
-				|> put_status(:ok)
-				|> json(%{message: "success"})
+    user_json_schema = generate_user_json_schema()
+    user_json_schema |> ExJsonSchema.Schema.resolve()
 
-			{:error, errors} -> 
-				conn
-				|> put_status(:bad_request)
-				|> render("user_errors.json", %{errors: errors})
-		end
-		
-		
-	end
+    case ExJsonSchema.Validator.validate(user_json_schema, params,
+           error_formatter: SimpleApp.JsonErrorFormatter
+         ) do
+      :ok ->
+        port = Application.get_env(:simple_app, :socket_port)
+        {:ok, socket} = :gen_tcp.connect('127.0.0.1', port, [:binary, packet: 0])
+        Logger.debug("socket ===> #{inspect(socket)}")
+        message = :gen_tcp.send(socket, params["name"] <> " " <> params["last_name"])
+        Logger.debug("message ===> #{inspect(message)}")
+        _ = :gen_tcp.close(socket)
 
-	#JSON SCHEMA
-	# name String required
-	# last_name String required
-	defp generate_user_json_schema() do
-		%{
-			"type" => "object",
-  		"properties" => %{
-    		"name" => %{
-      		"type" => "string"
-    		},
-	  		"last_name" => %{
-	  			"type" => "string"	      	
-	  		}	  		
-	  	},
-	  	"required" => ["name", "last_name"]
-		}
-	end
-	
+        conn
+        |> put_status(:ok)
+        |> json(%{message: "success"})
+
+      {:error, errors} ->
+        conn
+        |> put_status(:bad_request)
+        |> render("user_errors.json", %{errors: errors})
+    end
+  end
+
+  # JSON SCHEMA
+  # name String required
+  # last_name String required
+  defp generate_user_json_schema() do
+    %{
+      "type" => "object",
+      "properties" => %{
+        "name" => %{
+          "type" => "string"
+        },
+        "last_name" => %{
+          "type" => "string"
+        }
+      },
+      "required" => ["name", "last_name"]
+    }
+  end
 end
