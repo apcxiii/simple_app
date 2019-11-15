0be95fd9182c5dab3bf1fec5057d4a4ca80ae555
Erik Vera <apcxiii@gmail.com>
2019-11-14 19:09:52 -0600

    merge

---
+++ lib/tcp/tcp_example.ex
@@ -0,0 +1,27 @@
+defmodule SimpleApp.TcpExample do
+  require Logger
+  alias SimpleApp.Handler
+
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: 0, active: false, reuseaddr: true])
+
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Logger.info("Accpeted new connection")
+
+    {:ok, pid} =
+      DynamicSupervisor.start_child(SimpleApp.Handler.DynamicSupervisor, %{
+        id: Handler,
+        start: {Handler, :start_link, [%{socket: client}, []]},
+        type: :worker,
+        restart: :transient
+      })
+
+    :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+end
